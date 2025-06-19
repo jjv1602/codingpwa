7331107e6c0d4533225e35d37728bc8778df75b4
jjv1602 <joel.varghese2020@vitbhopal.ac.in>
2025-06-19 20:00:34 +0530

    pwa changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import nextPwa from "@ducanh2912/next-pwa";
+
+// const withPWA = require("@ducanh2912/next-pwa").default({
+//   dest: "public",
+// });
+
+// const withPWAFactory = require("@ducanh2912/next-pwa").default;
+
+
+const withPWAConfig = nextPwa({
+  dest: "public",
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+
+export default withPWAConfig(nextConfig);
